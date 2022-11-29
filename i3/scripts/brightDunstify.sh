409be782dad188918032618673b0f982ab093e5b
Evrim <ehotamis@gmail.com>
2022-11-29 16:43:38 +0100

    update

--- i3/scripts/brightDunstify.sh
+++ i3/scripts/brightDunstify.sh
@@ -1,7 +1,7 @@
 alias sendBacklight='dunstify -i "weather-clear" -h string:x-dunst-stack-tag:helderheid -t 1000 "Helderheid" $(xbacklight -get | cut -d '.' -f 1)'
 
 case $1 in
-	"up") xbacklight -inc 10;;
-	"down") xbacklight -dec 10;;
+	"up") xbacklight -inc 5;;
+	"down") xbacklight -dec 5;;
 esac
 sendBacklight
